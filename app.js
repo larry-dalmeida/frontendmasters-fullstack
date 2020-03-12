c1fc7fe624eb835e6661b60ed81c0f18836a6e28
Larry D'Almeida <hello@larrydalmeida.com>
2020-03-12 14:44:24 +0100

    Add Basic server

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
-const express = require('express')
-const app = express()
+const compression = require("compression");
+const express = require("express");
+
+const app = express();
+app.use(compression());
+
 const port = 3000
 
 app.use(express.static('ui'));
-app.get('/', (req, res) => res.send('Hello World!2'))
+app.get('/', (req, res) => res.send('Hello World!2'));
+app.get('/headers', (req, res) => {
+    res.status(418);
+    res.set('X-App-Test', 'Setting custom headers');
+    res.send('yo!');
+});
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
